0ed0df396eb13bd1b90594b9569dbc0bb2b56c34
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-11 16:57:16 +0800

    解釋資料夾說明

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,8 +1,14 @@
+// 從express輸入router
 const { Router } = require('express')
+
+// 輸入google and logout middleware
 const googleAuthRoutes = require('./google')
 const logoutRoutes = require('./logout')
+
+// 定義router
 const authRouter = Router()
 
+// 根據唔同的網址再入到router
 authRouter.use('/google', googleAuthRoutes)
 authRouter.use('/logout', logoutRoutes)
 
