33beb510493f62a4e53423015c3b93ee48910cd2
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-11 16:59:25 +0800

    google登入登出 (#12)
    * 先建立google登入和登出的仲介
    
    * 解釋資料夾說明

--- README.md
+++ README.md
@@ -33,3 +33,13 @@ npm run db:migrate
 ```
 npm run db:drop
 ```
+
+# 解釋資料夾內容
+
+-/api : 運行一個伺服器,將網址交比中介處理
+  -/migrations ： 利用3個特別功能去初始化資料夾
+  -/routes : api和auth的中介
+    -/api : 滙出api的中介
+      -/models : 同有資料庫有關的操作直接放這裏
+    -/auth : 同登入登出有關的仲介 
+ 
--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,4 +1,16 @@
-module.exports = (req, res, next) => {
-  console.log('hello auth')
-  next()
-}
+// 從express輸入router
+const { Router } = require('express')
+
+// 輸入google and logout middleware
+const googleAuthRoutes = require('./google')
+const logoutRoutes = require('./logout')
+
+// 定義router
+const authRouter = Router()
+
+// 根據唔同的網址再入到router
+authRouter.use('/google', googleAuthRoutes)
+authRouter.use('/logout', logoutRoutes)
+
+// 滙出一個Router,作為仲介者使用
+module.exports = authRouter
