c4c300699581ac595fd8f2f3a064d18823ee69af
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-11 16:41:27 +0800

    先建立google登入和登出的仲介

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,4 +1,10 @@
-module.exports = (req, res, next) => {
-  console.log('hello auth')
-  next()
-}
+const { Router } = require('express')
+const googleAuthRoutes = require('./google')
+const logoutRoutes = require('./logout')
+const authRouter = Router()
+
+authRouter.use('/google', googleAuthRoutes)
+authRouter.use('/logout', logoutRoutes)
+
+// 滙出一個Router,作為仲介者使用
+module.exports = authRouter
