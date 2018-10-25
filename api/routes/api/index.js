74b710433b03eb5faf1c3c84178783a04e0930a7
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-25 17:12:02 +0800

    首頁內容比用戶改and apiRoutes比用戶任意發揮 (#34)
    * 首頁內容比用戶改and apiRoutes比用戶任意發揮
    
    * released 0.0.11
    
    * examples versions updated

--- api/routes/api/index.js
+++ api/routes/api/index.js
@@ -1,8 +1,8 @@
-// 滙出一個function,作為仲介者使用
-// awit係等要同asnyc一起使用,而且要loading這一行再運行下面的東西
-module.exports = async (req, res, next) => {
-  const users = await req.models.getUsers()
-  console.log(users)
-  console.log('hello api')
-  next()
-}
+// 從express輸入router
+const { Router } = require('express')
+
+// 定義router
+const apiRouter = Router()
+
+// 滙出一個Router,作為仲介者使用
+module.exports = apiRouter
